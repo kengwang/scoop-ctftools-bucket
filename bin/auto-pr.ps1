15da87f6b6e61c308d9a93ce05fe4e12265eaa85
Kengwang <github@kengwang.com.cn>
2024-04-13 20:57:40 +0800

    add: Yakit

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "kengwang/scoop-security-bucket:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
